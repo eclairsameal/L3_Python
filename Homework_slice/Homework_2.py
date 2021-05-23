46ddf11fbc18c6e07877a67343977dedc87e31a7
Fenrir <31716348+eclairsameal@users.noreply.github.com>
2021-05-23 20:48:04 +0900

    Update Homework_2.py

--- Homework_slice/Homework_2.py
+++ Homework_slice/Homework_2.py
@@ -15,4 +15,10 @@ for i in range(n):
     r_l = random.randint(1, 100)
     random_list.append(r_l)
 print("Original:",random_list)
+
 print("Result:", sorted(random_list)[1:-1])
+
+"""
+random_list.sort()
+print("Result:", random_list[1:-1])
+"""
